a8c73ee39a551a10c1eb9143e9ce4452aca6b2cb
Atul Midha <atul.midda@g-axon.com>
2020-10-27 22:31:12 +0530

    updated redux

--- src/redux/ActionTypes.js
+++ src/redux/ActionTypes.js
@@ -1,4 +1,3 @@
 export const GET_COUNTRIES_LIST  = 'get_countries_list';
-export const DELETE_COUNTRY = 'delete_country';
 export const SET_SELECTED_DATA = 'set_selected_data';
 export const REMOVE_SELECTED_CITY = 'remove_selected_city';
